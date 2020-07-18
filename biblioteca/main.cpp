4de34b7ef2ac5abac33801f3be91144aefd3ad21
Junior R. Ribeiro-Akai <j5r@outlook.com>
2020-07-18 02:27:24 -0300

    closing: get | set | print

--- biblioteca/main.cpp
+++ biblioteca/main.cpp
@@ -4,12 +4,14 @@
 
 int main(){ 
   Matrix<float> m(2,4);
-  Matrix<double> n(2,13);
-  n.set(0,0,3.14159265358979);
+  Matrix<double> n(53,123);
+  n.set(-3,-2,3.14159265358979);
   std::cout << m.get(0,1) << "\n";
   m.set(1,0,-3.5);
   m.print(2);
-  n.print(2);
+  n.set(1,12,-112.91);
+  n.print();
+
   
   
   
--- biblioteca/matrix.cpp
+++ biblioteca/matrix.cpp
@@ -15,6 +15,7 @@ class Matrix {
     myself = (Type **) calloc(m,sizeof(Type *));
     if(myself == nullptr){
       cout << _FRED << "Memory allocation error." << _RST << endl;
+      throw 7;
       exit(7);
     }
     register int i;
@@ -22,6 +23,7 @@ class Matrix {
       myself[i] = (Type *) calloc(n, sizeof(Type));
       if(myself[i] == nullptr){
         cout << _FRED <<"Memory allocation error." << _RST << endl;            
+        throw 7;
         exit(7);
       }
     }
@@ -39,21 +41,50 @@ class Matrix {
 
 void print(int precision=4){
   register int i, j;
-  int setw_ = precision+3;
+  bool dotsrows, dotscols;
+  int setw_ = precision + 8;
 
-  cout << _FYELLOW;
-  for(j=0;j<this->ncols;j++){
-      cout << fixed << setprecision(precision) << setw(setw_) << setfill(' ')
-      << j+1 << "  ";
+  cout << _FYELLOW << setw(7) << setfill(' ') << right  << "Matrix";  
+  dotscols = (this->ncols > 6);
+  for(j=0; j<this->ncols; j++){
+    if((this->ncols>6) && (j>2 && j<this->ncols-3)){
+      if(dotscols){          
+        cout << left << setw(5) << "...";
+        dotscols = false;
+      }
+        continue;
     }
-  cout << _RST << endl;
+      cout << right << setprecision(precision) << setw(setw_) << setfill(' ');
+      cout << j + 1 << "  ";
+  }
+  cout << endl << _FBLUE;
+  
+  dotsrows = (this->nrows > 6);
+  for(i=0; i<this->nrows; i++){
+    dotscols = (this->ncols >6);    
+    if(this->nrows>6 && (i>2 && i<this->nrows-3)){
+      if(dotsrows){
+        cout << _FYELLOW <<setprecision(precision) << setw(7) << setfill(' ')
+            << right << ":";
+        cout << _FBLUE << setprecision(precision) << setw(setw_+1) << setfill(' ')
+            << right << ":\n";
+        dotsrows = false;
+      }
+      continue;
+    }
+    
+    cout << _FYELLOW << setw(7) << setfill(' ') << right  << i + 1 << _FBLUE;
 
-
-  cout << _FBLUE;
-  for(i=0;i<this->nrows;i++){
-    for(j=0;j<this->ncols;j++){
-      cout << fixed << setprecision(precision) << setw(setw_) << setfill(' ')
-      << myself[i][j] << "  ";
+    for(j=0; j<this->ncols; j++){      
+      if((this->ncols>6) && (j>2 && j<this->ncols-3)){
+        if(dotscols){          
+          cout << left << setw(5) << "...";
+          dotscols = false;
+        }
+        continue;
+      }
+      cout << scientific << right << setprecision(precision) << setw(setw_) << setfill(' ');
+      cout << myself[i][j] << "  ";
     }
     cout << endl;
   }
@@ -61,42 +92,24 @@ void print(int precision=4){
 }
 
 Type get(int i, int j){
+  i = (this->nrows + i )% this->nrows;
+  j = (this->ncols + j )% this->ncols;
+  if(i<0 || j<0){
+    throw 11;
+    exit(11);
+  }
   return myself[i][j];
 }
 
 void set(int i, int j, Type value){
+  i = (this->nrows + i )% this->nrows;
+  j = (this->ncols + j )% this->ncols;
+  if(i<0 || j<0){
+    throw 11;
+    exit(11);
+  }
   myself[i][j] = (Type) value;
 }
    
-  //void print();
+  
 };
-
-
-
-
-
-
-//
-    /*
-    Type get(int,int);
-    void set(int,int,int);
-    void set(int,int,float);
-    void set(int,int,double);
-
-
-    //
-    void _add(int);
-    void _add(float);
-    void _add(double);
-    void _add(Matrix);
-
-  */
-
-
-
-
-
-
-
-
-
